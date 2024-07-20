af1d667124139a76a9cdce2d92c210ca65d3d483
Jazpy <jaz.medtre@gmail.com>
2024-07-20 02:09:04 -0600

    Update example script

--- utilities/example_script.py
+++ utilities/example_script.py
@@ -1,10 +1,9 @@
-# Subject to change when package is finalized
-from src.simulation import Simulation
+from adnator.simulation import Simulation
 
 
 # Create simulation object with a configuration file
 sim = Simulation('utilities/example_config.yaml')
-# Run coalescent simulation (creates directories in CWD)
+# Run coalescent simulation (creates directories according to configuration file).
 sim.run_coalescent_simulation()
 # Run aDNA damage simulation
 sim.run_read_simulation()
