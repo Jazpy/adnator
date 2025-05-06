64a2059773bd831274cc294b7ff858aff1915fe6
Jazpy <jaz.medtre@gmail.com>
2025-05-06 12:38:40 -0600

    remove json dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements=[
 setup(
     name='adnator',
     install_requires=requirements,
-    version='1.2',
+    version='1.3',
     author='Jazeps Medina Tretmanis',
     author_email='jaz.medtre@gmail.com',
     description='A realistic-ish aDNA simulator',
